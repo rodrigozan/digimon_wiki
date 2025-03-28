eac4f0611db26801353a6e016b9cd35314b66fd9
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-03-28 10:42:43 -0300

    🆕 Add android to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,7 @@ export const environment = {
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
-  }
+  },
+  sha1Generate: "keytool -list -v -keystore C:\Users\rodrigo.zandonadi\.android\debug.keystore -alias androiddebugkey -storepass android -keypass android",
+  SHA1: "DD:37:92:1A:79:37:FD:97:4E:B1:D5:41:E0:E0:AD:BD:2A:41:2C:A3"
 };
